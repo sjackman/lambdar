cec82eb9d673f4e7d1b2bf3c366f795fc28a62e9
hb <hb@aroma-project.org>
2017-02-21 12:24:06 -0800

    Now local testing of lambdar is done using lambci/lambda-base

--- test-r.sh
+++ test-r.sh
@@ -2,5 +2,5 @@ tar xf r-${R_VERSION}.tar.gz -C /tmp
 export PATH=/tmp/r/${R_VERSION}/bin:${PATH}
 export LD_LIBRARY_PATH=/tmp/r/${R_VERSION}/lib64/R/lib
 Rscript -e "cat('R.home():', R.home(), '\n')"
-Rscript --version
+Rscript -e "sessionInfo()"
 if [[ ${INTERACTIVE} == "true" ]]; then bash; fi
