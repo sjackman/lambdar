00fa2cd05a9ba39f4f738e74004736508d58c416
hb <hb@aroma-project.org>
2017-02-21 12:47:55 -0800

    TEST: Test container has no home directory (~). PRUNING: Make R tarball even smaller

--- test-r.sh
+++ test-r.sh
@@ -3,4 +3,11 @@ export PATH=/tmp/r/${R_VERSION}/bin:${PATH}
 export LD_LIBRARY_PATH=/tmp/r/${R_VERSION}/lib64/R/lib
 Rscript -e "cat('R.home():', R.home(), '\n')"
 Rscript -e "sessionInfo()"
-if [[ ${INTERACTIVE} == "true" ]]; then bash; fi
+if [[ ${INTERACTIVE} == "true" ]]; then
+    export R_BIOC_VERSION=3.4
+    mkdir /tmp/test
+    cd /tmp/test
+    echo "R_BIOC_VERSION=${R_BIOC_VERSION}" >> .Renviron
+    echo "options(repos = c(CRAN='https://cloud.r-project.org', BioCsoft='https://bioconductor.org/packages/${R_BIOC_VERSION}/bioc', BioCann='https://bioconductor.org/packages/${R_BIOC_VERSION}/data/annotation', BioCexp='https://bioconductor.org/packages/${R_BIOC_VERSION}/data/experiment', BioCextra='https://bioconductor.org/packages/${R_BIOC_VERSION}/extra'))" >> .Rprofile
+    bash;
+fi
