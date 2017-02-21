6451471d91388fc8d76c203a127deb1e16252305
hb <hb@aroma-project.org>
2017-02-21 01:42:13 -0800

    An attempt on creating a minimal AWS Lambda R installation

--- lambdar.mk
+++ lambdar.mk
@@ -1,6 +1,7 @@
 # lambdar: Build R to run on AWS Lambda
 
 R_VERSION=3.3.2
+
 R_PREFIX=/tmp/r/$(R_VERSION)
 
 .DELETE_ON_ERROR:
@@ -15,10 +16,24 @@ all: r-$(R_VERSION).tar.gz
 	make; \
 	make install
 
+prune: /tmp/r/$(R_VERSION)
+	@rm -rf $(R_PREFIX)/share
+	@rm -f $(R_PREFIX)/lib64/R/COPYING $(R_PREFIX)/lib64/R/SVN-REVISION
+	@rm -rf $(R_PREFIX)/lib64/R/doc/*
+	@mkdir -p /tmp/r/3.3.2/lib64/R/doc/html ## install.packages()
+#	@rm -f $(R_PREFIX)/lib64/R/doc/NEWS*
+#	@rm -rf $(R_PREFIX)/lib64/R/doc/manual*
+	@rm -rf $(R_PREFIX)/lib64/R/library/*/doc
+	@rm -rf $(R_PREFIX)/lib64/R/library/*/html
+	@rm -rf $(R_PREFIX)/lib64/R/library/translations
+
 # Copy dependencies from amazonlinux-minimal-r build
 lib/%: /tmp/r/$(R_VERSION)
 	cp $(shell ldconfig -p | grep $(@F) | sed 's|.*=> ||g') $(R_PREFIX)/lib64/R/lib
 
 # Build the tarball of R and all of its dependencies
-r-%.tar.gz: /tmp/r/% lib/libgfortran.so.3 lib/libquadmath.so.0 lib/libgomp.so.1
+r-%.tar.gz: /tmp/r/% lib/libgfortran.so.3 lib/libquadmath.so.0 lib/libgomp.so.1 prune
 	tar zcf $@ -C /tmp r/$*
+
+clean:
+	@rm -rf $(R_PREFIX)
