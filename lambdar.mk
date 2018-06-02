eac9b46ba2fb22e94a2ddc7681c53c0340351ab3
hb <hb@aroma-project.org>
2018-06-02 16:49:13 -0700

    Further shrinking + placeholder for installing custom packages

--- lambdar.mk
+++ lambdar.mk
@@ -1,6 +1,7 @@
 # lambdar: Build R to run on AWS Lambda
 
 R_VERSION=3.5.0
+R_REPOS=https://cloud.r-project.org
 
 R_PREFIX=/tmp/r/$(R_VERSION)
 
@@ -16,6 +17,10 @@ all: r-$(R_VERSION).tar.gz
 	make; \
 	make install
 
+# Install custom packages here, if any
+pkgs: /tmp/r/$(R_VERSION)
+#	$(R_PREFIX)/bin/Rscript -e "install.packages(c('future'), repos ='$(R_REPOS)')"
+
 prune: /tmp/r/$(R_VERSION)
 	@rm -rf $(R_PREFIX)/share
 	@rm -f $(R_PREFIX)/lib64/R/COPYING $(R_PREFIX)/lib64/R/SVN-REVISION
@@ -25,15 +30,24 @@ prune: /tmp/r/$(R_VERSION)
 	@rm -rf $(R_PREFIX)/lib64/R/doc/manual*
 	@rm -rf $(R_PREFIX)/lib64/R/doc
 	@rm -rf $(R_PREFIX)/lib64/R/share/licenses/
+	@rm -f $(R_PREFIX)/lib64/R/library/*/INDEX
 	@rm -rf $(R_PREFIX)/lib64/R/library/*/demo
-	@rm -rf $(R_PREFIX)/lib64/R/library/*/Meta/demo.rds
-	@rm -rf $(R_PREFIX)/lib64/R/library/*/Meta/Rd.rds
+	@rm -f $(R_PREFIX)/lib64/R/library/*/Meta/demo.rds
+	@rm -f $(R_PREFIX)/lib64/R/library/*/Meta/hsearch.rds
+	@rm -f $(R_PREFIX)/lib64/R/library/*/Meta/links.rds
+	@rm -f $(R_PREFIX)/lib64/R/library/*/Meta/Rd.rds
+	@rm -f $(R_PREFIX)/lib64/R/library/*/Meta/vignette.rds
 	@rm -rf $(R_PREFIX)/lib64/R/library/*/doc
 	@rm -rf $(R_PREFIX)/lib64/R/library/*/html
 	@rm -rf $(R_PREFIX)/lib64/R/library/*/help
 	@rm -rf $(R_PREFIX)/lib64/R/library/translations
 	@ln -sf $(R_PREFIX)/lib64/R/bin/R $(R_PREFIX)/bin/R
 	@ln -sf $(R_PREFIX)/lib64/R/bin/Rscript $(R_PREFIX)/bin/Rscript
+	@rm -f $(R_PREFIX)/lib64/R/library/*/GPL*
+	@rm -f $(R_PREFIX)/lib64/R/library/*/NEWS*
+	@rm -f $(R_PREFIX)/lib64/R/library/*/CHANGELOG*
+	@rm -f $(R_PREFIX)/lib64/R/library/*/README*
+	@rm -rf $(R_PREFIX)/lib64/R/library/*/vignettes*
 
 
 # Copy dependencies from amazonlinux-minimal-r build
@@ -41,7 +55,7 @@ lib/%: /tmp/r/$(R_VERSION)
 	cp $(shell ldconfig -p | grep $(@F) | sed 's|.*=> ||g') $(R_PREFIX)/lib64/R/lib
 
 # Build the tarball of R and all of its dependencies
-r-%.tar.gz: /tmp/r/% lib/libgfortran.so.3 lib/libquadmath.so.0 lib/libgomp.so.1 prune
+r-%.tar.gz: /tmp/r/% lib/libgfortran.so.3 lib/libquadmath.so.0 lib/libgomp.so.1 pkgs prune
 	tar zcf $@ -C /tmp r/$*
 
 clean:
